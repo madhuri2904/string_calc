e74ba63439cd9fb24fc05e95a17aea09c203b40c
madhuri2904 <banoriyamadhuri@gmail.com>
2024-05-28 17:55:29 +0530

    Test case for '1'

--- spec/services/string_calc_spec.rb
+++ spec/services/string_calc_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe StringCalc do
 			expect(result).to eq(0)
 			puts "returns 0 for an empty string"
 		end
+
+		it 'returns sum for an string' do
+			result = StringCalc.add("1")
+			expect(result).to eq(1)
+			puts "returns 1 for an empty string"
+		end
 	end
 end
